6fda49433dfe22cb59b2167eb2e17c74fe2103b2
Cody Wahl <Cody.Wahl@greenwayhealth.com>
2015-06-04 16:45:51 -0400

    Added solution.

---
+++ EasyWmiTester/EasyWmiTester/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace EasyWmiTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run WMI query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(722, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(598, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(583, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(320, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Run Reg query";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(112, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(583, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(722, 187);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(714, 161);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "WMI Query";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WMI Query: ";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(714, 161);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "WMI Reg Query";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "sValueName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "sSubKeyName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 467);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EasyWmiTester/EasyWmiTester/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Windows.Forms;
+
+namespace EasyWmiTester
+{
+    public partial class Form1 : Form
+    {
+        public enum RegHive : uint
+        {
+            HKEY_CLASSES_ROOT = 0x80000000,
+            HKEY_CURRENT_USER = 0x80000001,
+            HKEY_LOCAL_MACHINE = 0x80000002,
+            HKEY_USERS = 0x80000003,
+            HKEY_CURRENT_CONFIG = 0x80000005
+        }
+
+        public enum RegType
+        {
+            REG_SZ = 1,
+            REG_EXPAND_SZ,
+            REG_BINARY,
+            REG_DWORD,
+            REG_MULTI_SZ = 7
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> RunWMIQuery(string query)
+        {
+            List<string> list = new List<string>();
+
+            string scope = @"\\.\root\CIMV2";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            try
+            {
+                ManagementObjectCollection collection = searcher.Get();
+
+                if (collection.Count == 0)
+                {
+                    MessageBox.Show("No results from query.");
+                }
+
+                foreach (ManagementObject mobj in collection)
+                {
+                    list.Add(mobj["name"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return list;
+        }
+
+        private string RunRegWMIQuery(string sSubKeyName, string sValueName)
+        {
+            try
+            {
+                ConnectionOptions oConn = new ConnectionOptions();
+                oConn.Impersonation = ImpersonationLevel.Impersonate;
+                oConn.EnablePrivileges = true;
+                oConn.Username = null;
+                oConn.Password = null;
+
+                string scopeString = @"\\.\root\default";
+
+                ManagementScope scope = new ManagementScope(scopeString, oConn);
+                ManagementPath myPath = new ManagementPath("StdRegProv");
+                ManagementClass registry = new ManagementClass(scope, myPath, null);
+
+                //object oValue = GetValue(registry, RegHive.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\.NETFramework", "InstallRoot");
+                object oValue = GetValue(registry, RegHive.HKEY_LOCAL_MACHINE, sSubKeyName, sValueName);
+                return oValue.ToString();
+
+                // Returns a specific value for a specified key
+                //ManagementBaseObject inParams = registry.GetMethodParameters("GetDWORDValue");
+                //const uint LOCAL_MACHINE = 0x80000002;
+                //inParams["hDefKey"] = hDefKey;
+                //inParams["hDefKey"] = LOCAL_MACHINE;
+                //inParams["sSubKeyName"] = sSubKeyName;
+                //inParams["sValueName"] = sValueName;
+
+                //inParams["hDefKey"] = 0x80000002;// HKEY_LOCAL_MACHINE;
+                //inParams["sSubKeyName"] = @"SOFTWARE\Microsoft\.NETFramework";
+                //inParams["sValueName"] = @"InstallRoot";
+
+                //ManagementBaseObject outParams = registry.InvokeMethod("GetDWORDValue", inParams, null);
+
+                //string output = "derp";
+
+                //foreach (var x in outParams.Properties)
+                //{
+                //    MessageBox.Show(x.Name);
+                //}
+
+                //if (outParams.Properties["sValue"].Value != null)
+                //{
+                //    output = outParams.Properties["sValue"].Value.ToString();
+                //}
+
+                //if (outParams.Properties["uValue"].Value != null)
+                //{
+                //    output = outParams.Properties["uValue"].Value.ToString();
+                //}
+
+                //if (outParams.Properties["ReturnValue"].Value != null)
+                //{
+                //    output = outParams.Properties["ReturnValue"].Value.ToString();
+                //}
+
+                //return "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return "";
+        }
+
+        public static object GetValue(ManagementClass mc, RegHive hDefKey, string sSubKeyName, string sValueName)
+        {
+            RegType rType = GetValueType(mc, hDefKey, sSubKeyName, sValueName);
+
+            ManagementBaseObject inParams = mc.GetMethodParameters("GetStringValue");
+            inParams["hDefKey"] = hDefKey;
+            inParams["sSubKeyName"] = sSubKeyName;
+            inParams["sValueName"] = sValueName;
+
+            object oValue = null;
+
+            switch (rType)
+            {
+                case RegType.REG_SZ:
+                    ManagementBaseObject outParams = mc.InvokeMethod("GetStringValue", inParams, null);
+
+                    if (Convert.ToUInt32(outParams["ReturnValue"]) == 0)
+                    {
+                        oValue = outParams["sValue"];
+                    }
+                    else
+                    {
+                        // GetStringValue call failed
+                    }
+                    break;
+
+                case RegType.REG_EXPAND_SZ:
+                    outParams = mc.InvokeMethod("GetExpandedStringValue", inParams, null);
+
+                    if (Convert.ToUInt32(outParams["ReturnValue"]) == 0)
+                    {
+                        oValue = outParams["sValue"];
+                    }
+                    else
+                    {
+                        // GetExpandedStringValue call failed
+                    }
+                    break;
+
+                case RegType.REG_MULTI_SZ:
+                    outParams = mc.InvokeMethod("GetMultiStringValue", inParams, null);
+
+                    if (Convert.ToUInt32(outParams["ReturnValue"]) == 0)
+                    {
+                        oValue = outParams["sValue"];
+                    }
+                    else
+                    {
+                        // GetMultiStringValue call failed
+                    }
+                    break;
+
+                case RegType.REG_DWORD:
+                    outParams = mc.InvokeMethod("GetDWORDValue", inParams, null);
+
+                    if (Convert.ToUInt32(outParams["ReturnValue"]) == 0)
+                    {
+                        oValue = outParams["uValue"];
+                    }
+                    else
+                    {
+                        // GetDWORDValue call failed
+                    }
+                    break;
+
+                case RegType.REG_BINARY:
+                    outParams = mc.InvokeMethod("GetBinaryValue", inParams, null);
+
+                    if (Convert.ToUInt32(outParams["ReturnValue"]) == 0)
+                    {
+                        oValue = outParams["uValue"] as byte[];
+                    }
+                    else
+                    {
+                        // GetBinaryValue call failed
+                    }
+                    break;
+            }
+
+            return oValue;
+        }
+
+        public static RegType GetValueType(ManagementClass mc, RegHive hDefKey, string sSubKeyName, string sValueName)
+        {
+            ManagementBaseObject inParams = mc.GetMethodParameters("EnumValues");
+            inParams["hDefKey"] = hDefKey;
+            inParams["sSubKeyName"] = sSubKeyName;
+
+            ManagementBaseObject outParams = mc.InvokeMethod("EnumValues", inParams, null);
+
+            if (Convert.ToUInt32(outParams["ReturnValue"]) == 0)
+            {
+                string[] sNames = outParams["sNames"] as String[];
+                int[] iTypes = outParams["Types"] as int[];
+
+                for (int i = 0; i < sNames.Length; i++)
+                {
+                    if (sNames[i] == sValueName)
+                    {
+                        return (RegType)iTypes[i];
+                    }
+                }
+                // value not found
+            }
+            else
+            {
+                // EnumValues call failed
+            }
+
+            throw new Exception("no return value");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource = null;
+            listBox1.Refresh();
+
+            listBox1.DataSource = RunWMIQuery(textBox1.Text);
+            listBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource = null;
+            listBox1.Refresh();
+
+            List<string> list = new List<string>();
+            list.Add(RunRegWMIQuery(textBox2.Text, textBox3.Text));
+
+            listBox1.DataSource = list;
+            listBox1.Refresh();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
